f9f43e81da26ba1a374ce81db3c2952a304b0c60
taewoo0522 <48813768+taewoo0522@users.noreply.github.com>
2019-04-26 10:02:22 +0900

    Update MyFrame.java

--- chap09-ex01/src/MyFrame.java
+++ chap09-ex01/src/MyFrame.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
 
-public class MyFrame {
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("ÇÁ·¹ÀÓ");
+		setSize(300, 300);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MyFrame frame = new MyFrame();
 
 	}
 
